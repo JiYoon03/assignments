987771546d42dbc3755f85472e2cf18c169e6560
JiYoon03 <riakangone0227@gmail.com>
2022-09-05 15:32:19 -0400

    assignment 0 exercise

---
+++ day2/largest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int largest(int arr[], int n) {
+  return 0;
+}
+
+int main() {
+  int values[10];
+  pritnf("Largest number is %d\n",largest(values,10));
+  return 0;
+}
