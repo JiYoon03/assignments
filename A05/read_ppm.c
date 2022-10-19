946da6cce5837abeac06f293a473aae34828a6b6
JiYoon Kang <jkang1@goldengate.cs.brynmawr.edu>
2022-10-19 02:49:15 -0400

    assignment complete

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -2,11 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include "read_ppm.h"
-
-// implement one
-
+//hexedit
+//filename
+//control C//to exit
+// Choose *one* to implement
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
-  return NULL;
+  // open the file to read
+  FILE* fr = fopen(filename, "rb");//read binary file in c: "rb"
+  if(!fr){//when filename is invalid
+    return NULL;
+  }
+  // allocate array for pixels
+  //ch2 pointer parameter
+  char line[1240];
+  fgets(line,1240,fr);//p6
+  printf("%s",line);
+  fgets(line,1240,fr);//#~~
+  printf("%s",line);
+  fscanf(fr,"%d %d\n",w,h);
+  printf("%d %d\n",*w,*h);
+  struct ppm_pixel* ppm = malloc(sizeof(struct ppm_pixel)*(*w)*(*h));
+  for(int i=0;i<(*w**h);i++){
+    ppm[i].red=0;
+    ppm[i].blue=0;
+    ppm[i].green=0;
+  }//don't need to do it bc fread initialized
+  fgets(line,1240,fr);
+  fread(ppm,sizeof(struct ppm_pixel),((*w)*(*h)),fr);
+  fclose(fr);
+  return ppm;
 }
 
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
--- A05/test_write_ppm.c
+++ A05/test_write_ppm.c
@@ -12,16 +12,14 @@ int main(int argc, char** argv) {
 
   free(pixels);
   pixels = read_ppm("feep-raw-test.ppm", &w, &h);
-
   printf("Testing file %s: %d %d\n", filename, w, h); 
   for (int r = 0; r < h; r++) {
     for (int c = 0; c < w; c++) {
        struct ppm_pixel p = pixels[r*w+c]; 
-       printf("(%u,%u,%u) ", p.red, p.green, p.blue); 
+       printf("(%d,%d,%d) ", p.red, p.green, p.blue); 
     }
     printf("\n");
   }
-
   free(pixels);
   return 0;
 }
