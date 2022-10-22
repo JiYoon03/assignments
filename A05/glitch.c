0978d4f03bec85d80fd54083e5279d717da919b7
JiYoon Kang <jkang1@goldengate.cs.brynmawr.edu>
2022-10-22 12:50:05 -0400

    assignment complete

--- A05/glitch.c
+++ A05/glitch.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include<time.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
 
@@ -9,9 +10,19 @@ int main(int argc, char** argv) {
     printf("usage: glitch <file.ppm>\n");
     return 0;
   }
-
-  // todo: your code here
-
+  int w,h;
+  srand(time(0));
+  struct ppm_pixel* pixels =read_ppm(argv[1],&w,&h);
+  printf("Reading %s with width %d and height %d",argv[0],606,771);
+  struct ppm_pixel* newcolorvalue = malloc(sizeof(struct ppm_pixel)*(606*771));
+  for(int i=0;i<(606*771);i++){
+    newcolorvalue[i].red = pixels[i].red << (rand() % 2);
+    newcolorvalue[i].green = pixels[i].green << (rand() % 2);
+    newcolorvalue[i].blue = pixels[i].blue << (rand() % 2);
+  }
+  write_ppm("monalisa-glitch.ppm",newcolorvalue, 606, 771);
+  free(pixels);
+  free(newcolorvalue);
   return 0;
 }
 
