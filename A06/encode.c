58fea21e86a847386274bfa9483cb498d1eb8737
JiYoon Kang <jkang1@goldengate.cs.brynmawr.edu>
2022-11-07 15:18:27 -0500

    assignment complete

--- A06/encode.c
+++ A06/encode.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
 
@@ -9,6 +10,11 @@ int main(int argc, char** argv) {
     printf("usage: encode <file.ppm>\n");
     return 0;
   }
+  string filename = argv[1];
+  int w,h;
+  struct ppm_pixel* pixels = read_ppm(argv[1], &w, &h);
+  Printf("Reading %s with width %d and height %d",filename,w,h);
+  
 
   return 0;
 }
